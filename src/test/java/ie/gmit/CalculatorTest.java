f715534f7bc5af51bd693b11718ab59f149e9ec3
Krystian <g00347897@gmit.ie>
2021-02-18 14:47:19 +0000

    addTest class, add function included in the build

--- src/test/java/ie/gmit/CalculatorTest.java
+++ src/test/java/ie/gmit/CalculatorTest.java
@@ -1,5 +1,14 @@
 package ie.gmit;
-import java.util.Scanner;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
+
+    @Test
+    public void testAdd(){
+        Calculator calculator = new Calculator();
+        double result = calculator.add(3.3, 2.2);
+        assertEquals(5.5, result, 0);
+    }
+
 }
